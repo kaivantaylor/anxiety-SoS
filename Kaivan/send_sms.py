93af7534dd3015dd906ad4cbb7065663d12b0976
speedykai <krt22452@email.vccs.edu>
2019-09-14 12:58:20 -0400

    f

--- Kaivan/send_sms.py
+++ Kaivan/send_sms.py
@@ -1,7 +1,7 @@
 # we import the Twilio client from the dependency we just installed
 from twilio.rest import Client
 from secret import sms_SID,sms_token
-from phone_numbers
+from phone_numbers import elijsha,rendell,kaivan,twillonumber
 
 # the following line needs your Twilio Account SID and Auth Token
 client = Client(sms_SID, sms_token)
@@ -9,6 +9,6 @@ client = Client(sms_SID, sms_token)
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
 # account to send SMS to any phone number
-client.messages.create(to="+16149352525",
-                       from_="+16143680178",
-                       body="test")
+client.messages.create(to=rendell,
+                       from_=twillonumber,
+                       body="Hello from Python!")
