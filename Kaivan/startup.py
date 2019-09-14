7515b63e53b1156e69b84eb8420b55c04c502e5b
speedykai <krt22452@email.vccs.edu>
2019-09-14 09:33:35 -0400

    added sleep() info and displayed in jupyter notebook

--- Kaivan/startup.py
+++ Kaivan/startup.py
@@ -17,3 +17,9 @@ ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
 REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
 
 auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+
+yesterday = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d"))
+yesterday2 = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
+today = str(datetime.datetime.now().strftime("%Y%m%d"))
+
+fit_statsSl = auth2_client.sleep()
