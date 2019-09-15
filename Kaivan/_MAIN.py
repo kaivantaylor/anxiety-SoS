b359778f40a7ea843ab982a79bf21011bab37e3e
speedykai <krt22452@email.vccs.edu>
2019-09-15 05:12:15 -0400

    formatting

--- Kaivan/_MAIN.py
+++ Kaivan/_MAIN.py
@@ -1,14 +1,13 @@
 # Loops infinitely every 30 seconds
 from update_CSV import update_CSV
-from main_func import check_anxiety
+from check_anxiety import check_anxiety
 import sched, time
 
-def infinite_update():
-    update_CSV()
-    check_anxiety()
+def run_loop():
     s = sched.scheduler(time.time, time.sleep)
     def do_something(sc):
         update_CSV()
+        check_anxiety()
         standard_msg += ht_msg
         s.enter(30, 1, do_something, (sc,))
 
@@ -17,4 +16,4 @@ def infinite_update():
 
     System.out.println("\n ------------------------------------------------")
 
-infinite_update()
+run_loop()
