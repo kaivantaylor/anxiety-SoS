4e70e753e3ed9b81f09db900e9f8cdfcd4a3a4a2
speedykai <krt22452@email.vccs.edu>
2019-09-14 22:25:42 -0400

    moving around things

---
+++ Kaivan/detect_anxiety.py
@@ -0,0 +1,3 @@
+from send_sms import send_sms
+
+send_sms()
--- Kaivan/send_sms.py
+++ Kaivan/send_sms.py
@@ -3,17 +3,18 @@ from twilio.rest import Client
 from secret import sms_SID,sms_token
 from secret import elijsha,rendell,kaivan,twillonumber
 
-# the following line needs your Twilio Account SID and Auth Token
-client = Client(sms_SID, sms_token)
+def send_sms():
+    # the following line needs your Twilio Account SID and Auth Token
+    client = Client(sms_SID, sms_token)
 
-message = "Check on Kaivan, his anxiety levels are detected as: high"
-# change the "from_" number to your Twilio number and the "to" number
-# to the phone number you signed up for Twilio with, or upgrade your
-# account to send SMS to any phone number
-client.messages.create(to=rendell,
-                       from_=twillonumber,
-                       body=message)
+    message = "Check on Rendell, his anxiety levels are detected as HIGH"
+    # change the "from_" number to your Twilio number and the "to" number
+    # to the phone number you signed up for Twilio with, or upgrade your
+    # account to send SMS to any phone number
+    client.messages.create(to=kaivan,
+                           from_=twillonumber,
+                           body=message)
 
-client.messages.create(to=elijsha,
-                       from_=twillonumber,
-                       body=message)
+    client.messages.create(to=elijsha,
+                           from_=twillonumber,
+                           body=message)
