078165750a00b6a0b91208348829e4f9226c40cb
speedykai <krt22452@email.vccs.edu>
2019-09-15 03:44:44 -0400

    add

--- Kaivan/main.py
+++ Kaivan/main.py
@@ -2,21 +2,43 @@ import csv
 import datetime
 from send_sms import send_sms
 
-list_stop = []
+heart_list = []
+bool_heart = False
+sleep_list = []
+bool_sleep = False
 
 with open(r"C:\\Users\speedykai\Desktop\hophacks2019\Kaivan\CSV\heartrate.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader: # row is the addresses individually from the csv csv_file
+    heart_rate = csv.reader(csv_file, delimiter=',')
+    for row in heart_rate: # row is the addresses individually from the csv csv_file
         try:
             a = int(row[1])
-            list_stop.append(a)
+            heart_list.append(a)
         except ValueError:
             pass
-        if len(list_stop) == 10:
+        if len(heart_list) == 10:
             break
 
-for heart_rate in list_stop:
-    if heart_rate < 90:
-        send_sms()
-    else:
-        pass
+for heart_rate in heart_list:
+    if heart_rate < 110:
+        bool_heart = True
+
+with open(r"C:\\Users\speedykai\Desktop\hophacks2019\Kaivan\CSV\sleep.csv") as csv_file:
+    sleep = csv.reader(csv_file, delimiter=',')
+    for row in sleep: # row is the addresses individually from the csv csv_file
+        print(row[2])
+        try:
+            b = row[2]
+            sleep_list.append(b)
+        except ValueError:
+            pass
+        if len(sleep_list) == 1:
+            break
+for sleep in sleep_list:
+    if int(sleep) < 300:
+        bool_sleep = True
+
+total = 0
+if bool_heart == True:
+    total += 40
+if bool_sleep == True:
+    total += 40
--- Kaivan/update_CSV.py
+++ Kaivan/update_CSV.py
@@ -27,6 +27,7 @@ def update_CSV():
 
     fit_statsHR = auth2_client.intraday_time_series('activities/heart')
     fitbit_stats2 = auth2_client.sleep(date=yesterday2)['sleep'][0]
+    fit_calorie = auth2_client.recent_foods()['calories'][0]
 
     stime_list = []
     sval_list = []
@@ -52,3 +53,9 @@ def update_CSV():
 
     ssummarydf.to_csv('sleep'+ \
                    '.csv', columns=['Date','Efficiency','Minutes Asleep','Time in Bed'], header=True, index=False, mode = 'a')
+
+    caloriesdf = pd.DataFrame({'Calories':fit_calorie['calories']
+                        },index=[0])
+
+    caloriesdf.to_csv('calories'+ \
+                   '.csv', columns=['Calories'], header=True, index=False, mode = 'a')
